0953c8a3d02555b1cde5c88065db49e7422601f0
Please <m.shakeel9038@gmail.com>
2024-10-14 13:38:59 +0530

    Added planned functionality for CurrencyConvertor

--- app/src/main/java/org/core/CurrencyConvertor.java
+++ app/src/main/java/org/core/CurrencyConvertor.java
@@ -4,13 +4,13 @@ import org.scrapers.IScraper;
 import org.core.Currency;
 
 public class CurrencyConvertor {
-    public CurrencyConvertor() {}
-
     public double convert(String src, String dest, double mnt_to_cnvrt) {
         return 2 * mnt_to_cnvrt;
     }
 
-    public void getCurrencyValue() {}
-
+    public void convertAll() {}
+    public void isValidCurrency() {}
+    public void search() {}
+    public void info() {}
     public void listCurrencies() {}
 }
